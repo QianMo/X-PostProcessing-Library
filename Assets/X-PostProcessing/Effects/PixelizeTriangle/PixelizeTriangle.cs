053d74c84fe394cb11ea0a9355c08ab2e26d5179
浅墨 <happylifemxy@163.com>
2020-05-24 22:31:32 +0800

    update description
    update description

--- Assets/X-PostProcessing/Effects/PixelizeTriangle/PixelizeTriangle.cs
+++ Assets/X-PostProcessing/Effects/PixelizeTriangle/PixelizeTriangle.cs
@@ -1,5 +1,6 @@
 ﻿//----------------------------------------------------------------------------------------------------------
 // X-PostProcessing Library
+// https://github.com/QianMo/X-PostProcessing-Library
 // Copyright (C) 2020 QianMo. All rights reserved.
 // Licensed under the MIT License 
 // You may not use this file except in compliance with the License.You may obtain a copy of the License at
